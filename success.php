fd71a913ef8078b604685a13af0eeab43457b580
gantik123 <138134133+gantik123@users.noreply.github.com>
2026-05-09 17:17:04 +0200

    Move packages to hero, update trades to May 2026, success.php link to ?start=paid

--- success.php
+++ success.php
@@ -216,7 +216,7 @@ if (!$token || strlen($token) !== 64) {
   <div class="error-icon">✕</div>
   <h1 class="error">LINK EXPIRED</h1>
   <p class="subtitle">This activation link has already been used or is invalid. Each link can only be opened once. If you believe this is a mistake, contact support.</p>
-  <a href="https://t.me/momentocrypto_bot" class="bot-btn">
+  <a href="https://t.me/momentocrypto_bot?start=paid" class="bot-btn">
     <svg width="20" height="20" viewBox="0 0 24 24" fill="none"><path d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm5.894 8.221-1.97 9.28c-.145.658-.537.818-1.084.508l-3-2.21-1.447 1.394c-.16.16-.295.295-.605.295l.213-3.053 5.56-5.023c.242-.213-.054-.333-.373-.12l-6.871 4.326-2.962-.924c-.643-.204-.657-.643.136-.953l11.57-4.461c.537-.194 1.006.131.833.941z" fill="currentColor"/></svg>
     CONTACT SUPPORT
   </a>
@@ -255,7 +255,7 @@ if (!$token || strlen($token) !== 64) {
     </div>
   </div>
 
-  <a href="https://t.me/momentocrypto_bot" class="bot-btn">
+  <a href="https://t.me/momentocrypto_bot?start=paid" class="bot-btn">
     <svg width="20" height="20" viewBox="0 0 24 24" fill="none"><path d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm5.894 8.221-1.97 9.28c-.145.658-.537.818-1.084.508l-3-2.21-1.447 1.394c-.16.16-.295.295-.605.295l.213-3.053 5.56-5.023c.242-.213-.054-.333-.373-.12l-6.871 4.326-2.962-.924c-.643-.204-.657-.643.136-.953l11.57-4.461c.537-.194 1.006.131.833.941z" fill="currentColor"/></svg>
     OPEN TELEGRAM BOT
   </a>
